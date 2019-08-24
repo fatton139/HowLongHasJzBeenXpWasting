a604cb649cbdf4792831c0120c9a69e06c1487c6
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-08-24 22:07:52 +1000

    Use db instead of local

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,6 @@
 import * as express from "express";
+import { MongoClient, Db, Collection } from "mongodb";
 import hiscores, { Player } from "osrs-json-hiscores";
-
-namespace Locals {
-    export let lastXpUpdate: Date = new Date();
-    export let lastUpdateData: Player;
-}
 
 const router = express.Router();
 
@@ -20,10 +16,6 @@ type ResponseWrapper = {
     xpDifferenceSinceUpdate: number;
 };
 
-router.get("/", async (req, res) => {
-    res.render("index");
-});
-
 const computeXpDifference = (newData: Player, oldData: Player): number => {
     return newData.main.skills.overall.xp - oldData.main.skills.overall.xp;
 };
@@ -37,38 +29,63 @@ const hasXpDifference = (newData: Player, oldData: Player): boolean => {
 
 const getFormattedDateTimeString = (date: Date): string => {
     return `${date.toDateString()}, ${date.toLocaleTimeString()}`;
-}
+};
+
+const getLastXpUpdate = async (): Promise<Date | undefined> => {
+    const data = await collection.findOne({});
+    return data.lastXpUpdate;
+};
+
+const getLastUpdateData = async (): Promise<Player | undefined> => {
+    const data = await collection.findOne({});
+    return data.lastUpdateData;
+};
+
+const getLastXpUpdateBreakdown = (currentDate: Date, lastXpUpdate: Date) => {
+    return {
+        hours: Math.floor(((currentDate as any) - (lastXpUpdate as any)) / (1000 * 60 * 60)),
+        minutes: Math.floor(((currentDate as any) - (lastXpUpdate as any)) / (1000 * 60)) % 60,
+    };
+};
+
+type PlayerData = {
+    lastXpUpdate: Date,
+    lastUpdateData: Player,
+};
+
+let collection: Collection<PlayerData>;
+
+router.get("/", async (req, res) => {
+    const client = await MongoClient.connect(process.env.DB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db("Cluster0");
+    collection = db.collection("data");
 
 router.get("/isJzXpWaste", async (req, res) => {
-    const currentDate: Date =  new Date();
-    const latestData: Player = await hiscores.getStats("Jz Cx");
+    const currentDate: Date = new Date();
+    const latestData: Player = await hiscores.getStats("Assasindie");
+    let lastXpUpdate = await getLastXpUpdate();
+    let lastUpdateData = await getLastUpdateData();
 
-    if (!Locals.lastXpUpdate) {
-        Locals.lastXpUpdate = currentDate;
+    if (hasXpDifference(latestData, lastUpdateData)) {
+        await collection.updateOne({}, { $set: {lastXpUpdate: currentDate}});
+        await collection.updateOne({}, { $set: {lastUpdateData: latestData}});
+        lastXpUpdate = currentDate;
+        lastUpdateData = latestData;
     }
 
-    if (!Locals.lastUpdateData) {
-        Locals.lastUpdateData = latestData;
-    }
-
-    if (hasXpDifference(latestData, Locals.lastUpdateData)) {
-        Locals.lastXpUpdate = currentDate;
-        Locals.lastUpdateData = latestData;
-    }
-
-    const lastXpUpdateBreakdown = {
-        hours: Math.floor(((currentDate as any) - (Locals.lastXpUpdate as any)) / (1000 * 60 * 60)),
-        minutes: Math.floor(((currentDate as any) - (Locals.lastXpUpdate as any)) / (1000 * 60)) % 60,
-    };
+    const lastXpUpdateBreakdown = getLastXpUpdateBreakdown(currentDate, lastXpUpdate);
 
     const responseWrapper: ResponseWrapper = {
         playerData: latestData,
-        lastXpUpdate: Locals.lastXpUpdate ? getFormattedDateTimeString(Locals.lastXpUpdate) : getFormattedDateTimeString(currentDate),
-        xpDifferenceSinceUpdate: computeXpDifference(Locals.lastUpdateData, latestData),
+        lastXpUpdate: getFormattedDateTimeString(lastXpUpdate),
+        xpDifferenceSinceUpdate: computeXpDifference(lastUpdateData, latestData),
         lastXpUpdateBreakdown,
     };
     res.send(JSON.stringify(responseWrapper));
-
 });
 
 export default router;
